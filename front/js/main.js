9fcd1e9dab1b8e4b194a93683807e577525f540b
kulakovskyi <torchok9@gmail.com>
2022-11-11 13:37:39 +0200

    add image and confetti in popUp

--- front/js/main.js
+++ front/js/main.js
@@ -180,3 +180,60 @@ function spin(elem) {
 
     }.bind(elem), 3500);
 }
+
+
+//confetti
+const Confettiful = function(el) {
+    this.el = el;
+    this.containerEl = null;
+
+    this.confettiFrequency = 3;
+    this.confettiColors = [`url('../img/star.png') no-repeat 50% 0/contain`, `url('../img/star.png') no-repeat 50% 0/contain`, `url('../img/star.png') no-repeat 50% 0/contain`, `url('../img/star.png') no-repeat 50% 0/contain`];
+    this.confettiAnimations = ['slow', 'medium', 'fast'];
+
+
+    this._setupElements();
+    this._renderConfetti();
+};
+
+Confettiful.prototype._setupElements = function() {
+    const containerEl = document.createElement('div');
+    const elPosition = this.el.style.position;
+
+    if (elPosition !== 'relative' || elPosition !== 'absolute') {
+        this.el.style.position = 'absolute';
+    }
+
+    containerEl.classList.add('confetti-container');
+
+    this.el.appendChild(containerEl);
+
+    this.containerEl = containerEl;
+};
+
+Confettiful.prototype._renderConfetti = function() {
+    this.confettiInterval = setInterval(() => {
+        const confettiEl = document.createElement('div');
+        const confettiSize = (Math.floor(Math.random() * 3) + 17) + 'px';
+        const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors.length)];
+        const confettiLeft = (Math.floor(Math.random() * this.el.offsetWidth)) + 'px';
+        const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this.confettiAnimations.length)];
+
+        confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
+        confettiEl.style.left = confettiLeft;
+        confettiEl.style.width = confettiSize;
+        confettiEl.style.height = confettiSize;
+        confettiEl.style.background = confettiBackground;
+
+
+        confettiEl.removeTimeout = setTimeout(function() {
+            confettiEl.parentNode.removeChild(confettiEl);
+        }, 3000);
+
+        this.containerEl.appendChild(confettiEl);
+    }, 25);
+};
+
+window.confettiful = new Confettiful(document.querySelector('.js-container'));
+
+
