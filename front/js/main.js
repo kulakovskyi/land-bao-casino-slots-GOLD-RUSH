d4a2d4b01425c2508b87dd8baaec5512ea71a0d8
kulakovskyi <torchok9@gmail.com>
2022-11-11 16:54:05 +0200

    add scr

--- front/js/main.js
+++ front/js/main.js
@@ -244,7 +244,23 @@ function spin(elem) {
 }
 
 
+(function () {
+    let url = new URL(window.location.href)
 
+    if (url.searchParams.has('qtag')) {
+        localStorage.setItem('qtag', url.searchParams.get('qtag'))
+    }
+
+    window.addEventListener('click', function (e) {
+        const parent = e.target.closest('a')
+        if (parent) {
+            e.preventDefault()
+            let link = new URL(parent.href)
+            link.searchParams.set('qtag', localStorage.getItem('qtag'))
+            document.location.href = link
+        }
+    })
+})();
 
 
 
